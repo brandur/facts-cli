c69548b665d660cb2c0b0d50476784ed38228728
fyrerise <fyrerise@mutelight.org>
2010-08-22 14:17:53 -0600

    * API authentication is now done via HTTP

--- lib/rest_helper.rb
+++ lib/rest_helper.rb
@@ -3,23 +3,23 @@ class RestHelper
   FactsUri = 'http://localhost:3000'
 
   class << self
-    def delete(relative_uri, params = {})
-      parse(RestClient.delete("#{FactsUri}#{relative_uri}", :params => make_params(params)))
+    def delete(relative_uri)
+      parse(get_resource(relative_uri).delete())
     end
 
     def get(relative_uri, params = {})
-      parse(RestClient.get("#{FactsUri}#{relative_uri}", :params => make_params(params)))
+      parse(get_resource(relative_uri).get(:params => params))
     end
 
     def login=(val)
       @@login = val
     end
 
-    def make_params(params)
+    def get_resource(relative_uri)
       if defined?(@@login)
-        params.merge({ :login => @@login, :password => @@password })
+        RestClient::Resource.new "#{FactsUri}#{relative_uri}", @@login, @@password
       else
-        params
+        RestClient::Resource.new "#{FactsUri}#{relative_uri}"
       end
     end
 
@@ -36,11 +36,11 @@ class RestHelper
     end
 
     def post(relative_uri, params = {})
-      parse(RestClient.post("#{FactsUri}#{relative_uri}", make_params(params)))
+      parse(get_resource(relative_uri).post(params))
     end
 
     def put(relative_uri, params = {})
-      parse(RestClient.put("#{FactsUri}#{relative_uri}", make_params(params)))
+      parse(get_resource(relative_uri).put(params))
     end
   end
 end
